26ad30430fb915f5ed8817bc72482bfcd58cb282
aina200 <ainapukipuki@gmail.com>
2020-11-30 18:54:05 +0100

    Form Validation start

--- app.js
+++ app.js
@@ -57,6 +57,122 @@ window.addEventListener('scroll', ()=>{
 
 
 
+//*****************************FORM VALIDATION ***************************************
+
+let formButton = document.getElementById('homePageFormButton');
+
+let lastName = document.querySelector('.nameInput');
+let lastNameError = document.getElementById('lastNameError');
+let regexName = /^([a-zA-Z ]+)$/;
+
+
+let email = document.querySelector('.emailInput');
+let emailError = document.getElementById('emailError');
+let regexEmail = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+
+let phone = document.querySelector('.phoneInput');
+let phoneError = document.getElementById('phoneError');
+let regexPhone =  /^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*$/;
+
+
+let textarea = document.querySelector('.textareaInput');
+let textareaError = document.getElementById('textareaError');
+
+
+
+formButton.addEventListener('click',validation);
+
+function validation (e)
+{
+    //***********************************EMPTY VALIDATION*****************
+    if (lastName.validity.valueMissing && email.validity.valueMissing 
+        && textarea.validity.valueMissing&& phone.validity.valueMissing) {
+
+        event.preventDefault();
+
+        lastNameError.textContent = '*Nom manquant*';
+        lastNameError.style.color = 'red';
+
+        emailError.textContent = '*Email manquant*';
+        emailError.style.color = 'red';
+
+        phoneError.textContent = '*Numéro manquant*';
+        phoneError.style.color = 'red';
+
+        textareaError.textContent = '*Veuillez remplir le champ*';
+        textareaError.style.color = 'red';
+    }
+    
+    //***********************************CARACTERES VALIDATION*****************
+
+    else if (regexName.test(lastName.value) === false) 
+    {
+        event.preventDefault();
+
+        lastNameError.textContent = '*Nom Ivalide*';
+        lastNameError.style.color = 'red';
+    }
+    else if (regexName.test(lastName.value) === true) 
+    {
+        event.preventDefault();
+
+        lastNameError.textContent = 'Nom valide';
+        lastNameError.style.color = 'green';
+    }
+    if (regexEmail.test(email.value) === false) 
+    {
+        event.preventDefault();
+
+        emailError.textContent = 'Email invalide';
+        emailError.style.color = 'red';
+    }
+    else if (regexEmail.test(email.value) === true) 
+    {
+        event.preventDefault();
+
+        emailError.textContent = 'Email valide';
+        emailError.style.color = 'green';
+    }
+     if (regexPhone.test(phone.value) === false) 
+    {
+        event.preventDefault();
+
+        phoneError.textContent = 'Numéro invalide';
+        phoneError.style.color = 'red';
+    }
+    else if (regexPhone.test(phone.value) === true) 
+    {
+        event.preventDefault();
+
+        phoneError.textContent = 'Numéro valide';
+        phoneError.style.color = 'green';
+    }
+
+
+    //***********************************LENGTH VALIDATION*****************
+    else if (lastName.value.length <=2 ) 
+    {
+        lastNameError.textContent = "*Veuillez entrer plus de deux caractères*";
+        lastNameError.style.color = 'red';
+    }
+
+    else if (email.value.length <10 ) {
+
+        emailError.textContent = '*Veuillez entrer plus de caractères*';
+        emailError.style.color = 'red';
+    }
+
+    // else
+    // {
+    //     alert('Merci! Je revienderai vers vous dans les meilleurs delais!');
+    // }
+
+    
+}
+
+
+
 
 
 
