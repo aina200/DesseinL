c271a607992eed01d9690cf709f7b231ea707c10
aina200 <ainapukipuki@gmail.com>
2020-12-14 18:56:46 +0100

    eee

--- core/routing.php
+++ core/routing.php
@@ -1,9 +1,7 @@
-<<<<<<< HEAD
 <?php
 
 $pages = [
-    // 'homepage' => './views/homepage.phtml',
-    'homepage' => './controllers/homepage_controller.php',
+     'homepage' => './controllers/homepage_controller.php',
     'projectsPage' => 'views/projectsPage.phtml',
     'about' => 'views/about.phtml',
     'contact' => 'views/contact.phtml',
@@ -16,21 +14,4 @@ $page = $pages['homepage'];
 if (isset($_GET['page']) && in_array($_GET['page'], array_keys($pages))) {
     $page = $pages[$_GET['page']];
 }
-=======
-<?php
 
-$pages = [
-    'homepage' => './views/homepage.phtml',
-    'projectsPage' => 'views/projectsPage.phtml',
-    'about' => 'views/about.phtml',
-    'contact' => 'views/contact.phtml',
-
-    
-];
-
-$page = $pages['homepage'];
-
-if (isset($_GET['page']) && in_array($_GET['page'], array_keys($pages))) {
-    $page = $pages[$_GET['page']];
-}
->>>>>>> 52eb82e... Media querys phone
